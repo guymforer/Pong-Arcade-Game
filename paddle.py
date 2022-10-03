38df229459be6d1430eaf1d18a81e1182545da8b
guymforer <guymforer@gmail.com>
2022-10-03 13:13:32 +0300

    new changes

---
+++ paddle.py
@@ -0,0 +1,21 @@
+import turtle
+
+
+from turtle import Turtle
+
+class Paddle:
+    def __init__(self,x,y) -> None:
+        self.paddle = Turtle()
+        self.paddle.shape("square")
+        self.paddle.color("white")
+        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
+        self.paddle.penup()
+        self.paddle.goto(x,y)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
